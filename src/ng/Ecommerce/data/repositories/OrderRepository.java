438bd9c385d7d896be34ca40611aa3a30eeb7a84
Praise Nwankwo <nwankwopraison@gmail.com>
2026-09-15 17:41:26 +0100

    update(repository) : order repository

---
+++ src/ng/Ecommerce/data/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package ng.Ecommerce.data.repositories;
+
+import ng.Ecommerce.data.models.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends MongoRepository<Order, String> {
+    List<Order> findByCustomerId(String customerId);
+}
